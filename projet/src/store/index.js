a933bbcddb91b1fd549ebb2c90b19a54f80185f3
Nicolas Meister <91722355+seidja@users.noreply.github.com>
2023-03-13 16:29:33 +0100

    début store

--- projet/src/store/index.js
+++ projet/src/store/index.js
@@ -5,12 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    mdpOrga: null, alias: null, currentHero: null, listeEquipe: null, currentEquipe: null, nomOrga: null, currentOrga: null
   },
   mutations: {
+    setMdpOrga(state, mdpOrga) {
+      state.mdpOrga = mdpOrga
+    },
+    setAlias(state, alias) {
+        state.alias = alias
+    },
+    setCurrentHero(state, currentHero) {
+        state.currentHero = currentHero
+    },
+    setListeEquipe(state, listeEquipe) {
+        state.listeEquipe = listeEquipe
+    },
+    setCurrentEquipe(state, currentEquipe) {
+        state.currentEquipe = currentEquipe
+    },
+    setNomOrga(state, nomOrga) {
+        state.nomOrga = nomOrga
+    },
+    setCurrentOrga(state, currentOrga) {
+        state.currentOrga = currentOrga
+    }
   },
   actions: {
+  },
+  getters: {
+
   },
   modules: {
   }
