2d52a4af15f049e9c057b4e2ea09f3b563b7efd3
Nicolas Meister <91722355+seidja@users.noreply.github.com>
2023-03-27 15:19:55 +0200

    Store

--- projet/src/store/index.js
+++ projet/src/store/index.js
@@ -31,10 +31,126 @@ export default new Vuex.Store({
     }
   },
   actions: {
-  },
-  getters: {
+    registerOrga({commit}, mdpOrga) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+        method: 'POST', headers: {
+          'Content-Type': 'application/json'
+        }, body: JSON.stringify(mdpOrga)
+      })
+          .then(response => response.json())
+          .then(response => {
+            commit('setMdpOrga', response.data)
+          })
+          .catch(error => console.log('Error:', error))
+    }, getAlias({commit}, alias) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+        method: 'GET', headers: {
+          'Content-Type': 'application/json'
+        }, body: JSON.stringify(alias)
+      })
+          .then(response => response.json())
+          .then(response => {
+            commit('setAlias', response.data)
+          })
+          .catch(error => console.log('Error:', error))
+    }, getCurrentHero({commit}, currentHero) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+        method: 'GET', headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify(currentHero)
+        })
+            .then(response => response.json())
+            .then(response => {
+                commit('setCurrentHero', response.data)
+            })
+            .catch(error => console.log('Error:', error))
+    }, updateCurrentHero({commit}, currentHero) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+        method: 'PUT', headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify(currentHero)
+        })
+            .then(response => response.json())
+            .then(response => {
+                commit('setCurrentHero', response.data)
+            })
+            .catch(error => console.log('Error:', error))
+      }, getListeEquipe({commit}, listeEquipe) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+          method: 'GET', headers: {
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(listeEquipe)
+        })
+            .then(response => response.json())
+            .then(response => {
+                commit('setListeEquipe', response.data)
+            })
+            .catch(error => console.log('Error:', error))
+      }, getCurrentEquipe({commit}, currentEquipe) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+            method: 'GET', headers: {
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(currentEquipe)
+        })
+            .then(response => response.json())
+            .then(response => {
+                commit('setCurrentEquipe', response.data)
+            })
+            .catch(error => console.log('Error:', error))
+      }, updateCurrentTeam({commit}, currentEquipe) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+            method: 'PUT', headers: {
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(currentEquipe)
+        })
+            .then(response => response.json())
+            .then(response => {
+                commit('setCurrentEquipe', response.data)
+            })
+            .catch(error => console.log('Error:', error))
+      }, getNomOrga({commit}, nomOrga) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+            method: 'GET', headers: {
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(nomOrga)
+        })
+            .then(response => response.json())
+            .then(response => {
+                commit('setNomOrga', response.data)
+            })
+            .catch(error => console.log('Error:', error))
+      }, getCurrentOrga({commit}, currentOrga) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+            method: 'GET', headers: {
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(currentOrga)
+        })
+            .then(response => response.json())
+            .then(response => {
+                commit('setCurrentOrga', response.data)
+            })
+            .catch(error => console.log('Error:', error))
+      }, updateCurrentOrga({commit}, currentOrga) {
+      return fetch('https://apidemo.iut-bm.univ-fcomte.fr/herocorp/orgs/getbyid/63bfe549458c2ed0e63ac4f7?org-secret=nous%20sommes%20mechants', {
+            method: 'PUT', headers: {
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(currentOrga)
+        })
+            .then(response => response.json())
+            .then(response => {
+                commit('setCurrentOrga', response.data)
+            })
+            .catch(error => console.log('Error:', error))
+      }
 
   },
-  modules: {
+  getters: {
+    getAlias: state => state.alias,
+    getCurrentHero: state => state.currentHero,
+    getListeEquipe: state => state.listeEquipe,
+    getCurrentEquipe: state => state.currentEquipe,
+    getNomOrga: state => state.nomOrga,
+    getCurrentOrga: state => state.currentOrga
+
   }
 })
