ab0d119f75589f4753369a63680775e0767a72d9
ehyostepbro <80098893+valow-psd@users.noreply.github.com>
2023-03-27 15:34:45 +0200

    Réglage des fautes sur Heroes / hero / hereos etc...

--- projet/src/views/HeroesView.vue
+++ projet/src/views/HeroesView.vue
@@ -1,12 +1,16 @@
 <template>
     <v-container>
-      <p>heroes</p>
-    </v-container>
+    <v-app-bar>
+      <v-app-bar-title>
+        Heroes
+      </v-app-bar-title>
+    </v-app-bar>
+  </v-container>
   </template>
   
   <script>
   export default {
-    name: 'OrganisationView',
+    name: 'HeroesView',
   }
   </script>
   
