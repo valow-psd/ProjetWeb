f3549352d11a09b05af07b7aef368e8fefa2aa88
laval <baptiste.laval@edu.univ-fcomte.fr>
2023-03-27 15:29:07 +0200

    "ajout d'un text field dans organisation "

--- projet/src/views/OrganisationView.vue
+++ projet/src/views/OrganisationView.vue
@@ -1,17 +1,26 @@
 <template>
-    <v-container>
+  <v-container>
     <v-app-bar>
       <v-app-bar-title>
         Organisations
       </v-app-bar-title>
     </v-app-bar>
+
+    <v-text-field 
+    v-model="phrase_secrete" 
+    label="Entrez la phrase secrete" >
+    </v-text-field>
+    <span>{{ phrase_secrete }}</span>
   </v-container>
-  </template>
+</template>
   
-  <script>
-  export default {
-    name: 'OrganisationView',
-  }
-  </script>
+<script>
+export default {
+  name: 'OrganisationView',
+  data : () => ({
+    phrase_secrete : "oui"
+  })
+}
+</script>
   
   
