b028151a616c0b27c0d1a95ab83170b8ba23db12
ehyostepbro <80098893+valow-psd@users.noreply.github.com>
2023-04-01 10:06:48 +0200

    mot de passe fonctionnel

--- projet/src/views/LoginView.vue
+++ projet/src/views/LoginView.vue
@@ -3,13 +3,20 @@
   <v-card>
     <v-card-title>Se connecter</v-card-title>
     <v-card-text>
+      <p>Le mot de passe est <strong>nous sommes mechants
+      </strong></p>
       <v-form @submit.prevent="connexion">
         <v-text-field
           v-model="mdp"
           Label="Entrez le mot de passe"
-          type="password"
+          type="showPassword ? 'text' : 'password'"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="passwordRules"
           required
+          @click:append="showPassword = !showPassword"
         ></v-text-field>
+        <br>
         <v-btn type="submit">Se connecter</v-btn>
       </v-form>
     </v-card-text>
@@ -21,13 +28,18 @@
 export default {
   data(){
     return {
-      mdp : ""
+      mdp : "",
+      showPassword: false,
+      passwordRules: [
+        v => !!v || 'Un mot de passe est requis',
+        v => (v && v.length >= 8) || 'Minimum 8 caractères',
+      ]
     }
   },
   methods: {
-    se_connecter(){
+    connexion(){
       this.$store.commit('setMdpOrga', this.mdp)
-      this.$router.push('/orgs')
+      this.$router.push('/Organisation')
     }
   },
   name: "LoginView"
